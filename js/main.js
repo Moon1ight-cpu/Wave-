58a21311c67213ad37643cd24091efcebf81e779
Андрей <moon1ight2013@yandex.ru>
2022-04-05 22:27:45 +0300

    Сделан адаптив Шапки

--- js/main.js
+++ js/main.js
@@ -6,3 +6,7 @@ $(".slider-blog__inner").slick({
     arrows: false,
     dots: true,
 })
+
+$('.header__button').on('click',function() {
+    $('ul.menu__list').toggleClass('menu__list--active')
+})
